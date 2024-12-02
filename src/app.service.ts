5e941185d81a76850e7ec482fdec763a2d9d319e
Matias Torres <matias.torres@reserv.com.ar>
2024-12-02 18:09:00 -0300

    connect database postgres, pg admin, config global module whit postgresSql

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,31 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import config from './config';
+import { Client } from 'pg';
 
 @Injectable()
 export class AppService {
   constructor(
     // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
+    @Inject('PG') private ciientPg: Client,
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
   getHello(): string {
     const apiKey = this.configService.apiKey;
     const name = this.configService.database.name;
     return `Hello World! ${apiKey} ${name}`;
+  }
+
+  getTasks() {
+    return new Promise((resolve, reject) => {
+      this.ciientPg.query('SELECT * FROM tasks', (err, res) => {
+        console.error(err);
+        if (err) {
+          reject(err);
+        }
+        resolve(res.rows);
+      });
+    });
   }
 }
