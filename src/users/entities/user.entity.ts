579807756a4fc4b397b9bdfc62aef0ad7e7b42a4
matias.torres <mtri21019@gmail.com>
2024-12-17 20:10:04 -0300

    finish relation one to one

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,14 @@
-import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+import {
+  Column,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { Customer } from './customer.entity';
 
 @Entity()
 export class User {
@@ -13,4 +23,14 @@ export class User {
 
   @Column({ type: 'varchar', length: 50 })
   role: string;
+
+  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  createAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  updateAt: Date;
+
+  @OneToOne(() => Customer, (customer) => customer.user, { nullable: true })
+  @JoinColumn()
+  customer: Customer;
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,11 +6,12 @@ import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 import { CustomersService } from './services/customers.service';
 import { User } from './entities/user.entity';
+import { Customer } from './entities/customer.entity';
 
 import { ProductsModule } from '../products/products.module';
 
 @Module({
-  imports: [ProductsModule, TypeOrmModule.forFeature([User])],
+  imports: [ProductsModule, TypeOrmModule.forFeature([User, Customer])],
   controllers: [CustomerController, UsersController],
   providers: [CustomersService, UsersService],
 })
