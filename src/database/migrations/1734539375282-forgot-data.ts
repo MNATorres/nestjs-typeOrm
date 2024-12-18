64c6d9b76cf2e0f5e890acb41de446a3d0283635
matias.torres <mtri21019@gmail.com>
2024-12-18 17:12:52 -0300

    comment mysql docker

---
+++ src/database/migrations/1734539375282-forgot-data.ts
@@ -0,0 +1,24 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class forgotData1734539375282 implements MigrationInterface {
+    name = 'forgotData1734539375282'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`COMMENT ON COLUMN "product"."createAt" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "product"."updateAt" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "user"."createAt" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "user"."updateAt" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "customer"."createAt" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "customer"."updateAt" IS NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`COMMENT ON COLUMN "customer"."updateAt" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "customer"."createAt" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "user"."updateAt" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "user"."createAt" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "product"."updateAt" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "product"."createAt" IS NULL`);
+    }
+
+}
